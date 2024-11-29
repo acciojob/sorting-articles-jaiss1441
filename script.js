7273f2447d1f56b63dee5ab401d730f00c2eb145
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-30 02:37:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const Band = document.getElementById("band");
+
+bands.forEach(function (item) {
+	const li = document.createElement("li");
+	 li.textContent = item;
+	Band.appendChild(li);
+	
+});
